dc1620d9c55e0e940ac9f431cc82db6c144e7015
unknown <colbydude@gmail.com>
2015-07-26 18:53:14 -0700

    Add tile animations.

--- ZeldaEngine/Maps/Tile.cs
+++ ZeldaEngine/Maps/Tile.cs
@@ -18,8 +18,10 @@ namespace ZeldaEngine.Maps
         public int YPos { get; set; }
         public int ZPos { get; set; }
 
-        public int TextureXPos { get; set; }
-        public int TextureYPos { get; set; }
+        public List<TileFrame> TileFrames { get; set; }
+        public int AnimationSpeed { get; set; }
+        private double _counter;
+        private int _animationIndex;
 
         public string TextureName { get; set; }
         private Texture2D _texture;
@@ -29,13 +31,15 @@ namespace ZeldaEngine.Maps
 
         }
 
-        public Tile(int xPos, int yPos, int zPos, int textureXPos, int textureYPos, string textureName)
+        public Tile(int xPos, int yPos, int zPos, List<TileFrame> tileFrames, int animationSpeed, string textureName)
         {
             XPos = xPos;
             YPos = yPos;
             ZPos = zPos;
-            TextureXPos = textureXPos;
-            TextureYPos = textureYPos;
+            TileFrames = tileFrames;
+            AnimationSpeed = animationSpeed;
+            _counter = 0;
+            _animationIndex = 0;
             TextureName = textureName;
         }
 
@@ -46,12 +50,25 @@ namespace ZeldaEngine.Maps
 
         public void Update(double gameTime)
         {
-            
+            if (TileFrames.Count == 1)
+                return;
+
+            _counter += gameTime;
+            if (_counter > AnimationSpeed)
+            {
+                _counter = 0;
+                _animationIndex++;
+                if (_animationIndex >= TileFrames.Count)
+                    _animationIndex = 0;
+            }
         }
 
         public void Draw(SpriteBatch SpriteBatch)
         {
-            SpriteBatch.Draw(_texture, new Rectangle(XPos * Width, YPos * Height, Width, Height), new Rectangle(TextureXPos * (Width + 1) + 1, TextureYPos * (Height + 1) + 1, Width, Height), Color.White);
+            SpriteBatch.Draw(_texture,
+                             new Rectangle(XPos * Width, YPos * Height, Width, Height),
+                             new Rectangle(TileFrames[_animationIndex].TextureXPos * (Width + 1) + 1, TileFrames[_animationIndex].TextureYPos * (Height + 1) + 1, Width, Height),
+                             Color.White);
         }
     }
 }
